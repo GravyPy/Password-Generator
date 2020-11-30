1520c3b8fbd404f8bd58fbbcf4046ae4fef18e0c
GravyPy <mattfrisch123@gmail.com>
2020-11-29 19:25:39 -0600

    Created Basic Password Generator
    Beta Version of the Password Generator. Currently generates passwords anywhere from 8-32 characters. Also has the ability to store all passwords into a Text file on the users desktop

---
+++ passwordgen.py
@@ -0,0 +1,43 @@
+import string
+import random
+import time
+import os
+import os.path
+
+# Defining All Possible Characters
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&()*+,-./:;<=>?@[\]^_`{|}~0123456789"
+
+# Creates Empty Password Variable to Be Filled Later
+password = ''
+
+# Gathers User Input for Text File
+passwordApp = input("What App is this Password for? ")
+while True:
+    try:
+        passwordLength = int(input("How long do you want your password to be? (Enter a number) "))
+        assert 8 <= passwordLength <= 32
+    except ValueError:
+        print("Please enter a number!")
+    except AssertionError:
+        print("Number must be between 8 & 32 characters")
+    else:
+        break
+
+# Password Generation
+print("Generating Password")
+time.sleep(3)
+# Gets Password Length and Assigns Amount of Chars based on length
+for x in range(passwordLength):
+    password += random.choice(chars)
+print("Password Generated")
+time.sleep(2)
+print("Creating File")
+# Creates File on Users Desktop to Store Passwords
+passwordFile = open(os.path.expanduser(os.path.join('~/Desktop/Passwords.txt')), "a")
+passwordFile.write(passwordApp + "\n" + password + "\n")
+time.sleep(2)
+print("File Saved")
+time.sleep(1.5)
+# Thank You
+print("Thank You For Using The GravyGen")
+time.sleep(1.5)
